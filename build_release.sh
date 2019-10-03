d11d702c7439215ed5bec75ff8e186be95587859
tuxuser <462620+tuxuser@users.noreply.github.com>
2019-10-03 03:55:10 +0200

    Update to .NET Core 3.0 && Use framework-dependant executables

--- build_release.sh
+++ build_release.sh
@@ -1,43 +1,37 @@
-if [ ! $# -eq 3 ]; then
-  echo "Usage: ./build_release [RID] [TAG] [TARGET FRAMEWORK]"
+if [ ! $# -eq 2 ]; then
+  echo "Usage: ./build_release [RID] [TAG]"
   echo "Example:"
   echo "RID: for example: win-x64, win-x86, linux-x64, osx.10.12-x64"
   echo "TAG: A TAG to include in the release archive filename"
-  echo "TARGET FRAMEWORK: for example: netcoreapp2.0"
   exit 1
 fi
 
-PUBLISH_TARGET=$1
+RID=$1
 TAG_NAME=$2
-TARGET_FRAMEWORK=$3
 PROJ_ROOT=$(pwd)
-PUBLISH_ARGS="/p:TrimUnusedDependencies=true"
+
+PUBLISH_ARGS="/p:PublishSingleFile=true"
 
 # Cleanup first
 dotnet clean
+rm -fR publish-$RID
+rm -fr release/XVDTool-$RID-$TAG_NAME.zip
+
+mkdir release
 
 # Build core library
-rm -fR LibXboxOne/publish-$PUBLISH_TARGET
-dotnet publish $PUBLISH_ARGS -c Release -f netstandard2.0 -r $PUBLISH_TARGET -o publish-$PUBLISH_TARGET LibXboxOne
+# dotnet publish -c Release -r $RID -f netstandard2.1 -o publish-$RID LibXboxOne
 
 # Publish builds
-mkdir -p release/$PUBLISH_TARGET
-rm -fR release/$PUBLISH_TARGET/*
 for proj in {XVDTool,XBFSTool,DurangoKeyExtractor}
 do
-    rm -fR $proj/publish-$PUBLISH_TARGET
-    dotnet publish $PUBLISH_ARGS -c Release -f $TARGET_FRAMEWORK -r $PUBLISH_TARGET -o publish-$PUBLISH_TARGET $proj
-    cp -R $proj/publish-$PUBLISH_TARGET/* release/$PUBLISH_TARGET/
+    dotnet publish $PUBLISH_ARGS -c Release -r $RID -f netcoreapp3.0 -o publish-$RID $proj --self-contained false
 done
 
 # Bundle additional files
-cp README.md release/$PUBLISH_TARGET/
-cp CHANGELOG.md release/$PUBLISH_TARGET/
+cp README.md publish-$RID/
+cp CHANGELOG.md publish-$RID/
 
 # Package up
-cd release/$PUBLISH_TARGET
-zip -r ../XVDTool-$PUBLISH_TARGET-$TAG_NAME.zip .
-
-# Cleanup
-cd $PROJ_ROOT
-rm -fR release/$PUBLISH_TARGET
+cd publish-$RID
+zip -r ../release/XVDTool-$RID-$TAG_NAME.zip .
